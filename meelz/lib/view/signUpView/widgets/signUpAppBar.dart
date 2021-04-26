77567c1f5a8e8b80ec0f9b3f6d275374369dc67f
Amer Beso <abeso1@etf.unsa.ba>
2021-04-26 17:23:46 +0200

    sign up app bar almost done

--- meelz/lib/view/signUpView/widgets/signUpAppBar.dart
+++ meelz/lib/view/signUpView/widgets/signUpAppBar.dart
@@ -4,10 +4,28 @@ import 'package:flutter_svg/svg.dart';
 ///appbar for sign up screen
 Widget signUpAppBar(BuildContext context) {
   return AppBar(
-    leading: SvgPicture.asset(
-      "assets/images/logo.svg",
-      height: 30.56,
-      width: 74,
+    backgroundColor: Colors.transparent,
+    leadingWidth: 94,
+    elevation: 0,
+    leading: Padding(
+      //in layout there is 20 pixel margin to left
+      padding: const EdgeInsets.only(
+        left: 20,
+      ),
+      //logo of meelz
+      child: SvgPicture.asset(
+        "assets/images/logo.svg",
+        height: 30.56,
+        width: 74,
+      ),
     ),
+    actions: [
+      RaisedButton(
+        color: Colors.transparent,
+        elevation: 0,
+        onPressed: () {},
+        child: Text("SKIP"),
+      )
+    ],
   );
 }
