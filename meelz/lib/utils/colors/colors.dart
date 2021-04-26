400640b9a5163bb954e693ee98ed7d5a768ff021
Amer Beso <abeso1@etf.unsa.ba>
2021-04-26 16:20:27 +0200

    primary color setted

---
+++ meelz/lib/utils/colors/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryColor = const MaterialColor(
+  0xFFFFDF36,
+  const <int, Color>{
+    50: const Color(0xFFFFDF36),
+    100: const Color(0xFFFFDF36),
+    200: const Color(0xFFFFDF36),
+    300: const Color(0xFFFFDF36),
+    400: const Color(0xFFFFDF36),
+    500: const Color(0xFFFFDF36),
+    600: const Color(0xFFFFDF36),
+    700: const Color(0xFFFFDF36),
+    800: const Color(0xFFFFDF36),
+    900: const Color(0xFFFFDF36),
+  },
+);
